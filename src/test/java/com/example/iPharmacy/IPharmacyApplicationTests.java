609b42a8b923c6b502e5cced9e1a5e883ef5f44f
anthonyz713 <78340967+anthonyz713@users.noreply.github.com>
2021-09-15 23:39:37 -0700

    Web service template

---
+++ src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iPharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

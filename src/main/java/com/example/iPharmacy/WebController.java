21ff6c30bcdd28cb9eff649caa7e0c1ad70ce33e
Siwen_W <u.robbinn@gmail.com>
2021-09-19 00:31:13 -0700

    for A4 q1 and q2

---
+++ src/main/java/com/example/iPharmacy/WebController.java
@@ -0,0 +1,33 @@
+package com.example.iPharmacy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    public static List<String> list= new ArrayList<>();
+
+    static {
+        list.add("btn btn-danger");
+        list.add("btn btn-success");
+        list.add("btn btn-info");
+        list.add("btn btn-secondary");
+        list.add("btn btn-warning");
+        list.add("btn btn-dark");
+    }
+
+    @GetMapping("/button")
+    public String changeButton(Model model){
+
+        // generating the index using Math.random()
+        int index = (int)(Math.random() * list.size());
+
+        model.addAttribute("color", list.get(index));
+        return "thymeleaftest";
+    }
+}
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -20,7 +20,6 @@ function createAccount() {
     }
     });
   }
-  
 }
 
 function getRandom() {

81497163f409edd7026e72145748037a4c7b26d3
ana569 <83257568+ana569@users.noreply.github.com>
2021-09-23 22:50:31 -0700

    Add files via upload

---
+++ src/main/java/com/example/iPharmacy/ReadFileController.java
@@ -0,0 +1,40 @@
+package com.example.iPharmacy;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+
+@RestController
+public class ReadFileController {
+    File myfile = new File("src/main/resources/words.txt");
+
+    String contents = FileUtils.readFileToString(myfile,
+            StandardCharsets.UTF_8.name());
+
+    public ReadFileController() throws IOException {
+    }
+
+    @RequestMapping(value="/read-file", method = RequestMethod.GET )
+public String file() throws IOException {
+            return contents;
+        }
+        @RequestMapping(value="/read-file/numlines", method = RequestMethod.GET )
+    public String numlines() throws IOException {
+            List lines = FileUtils.readLines(myfile,
+                    StandardCharsets.UTF_8.name());
+            return "Number of lines in file: "+lines.size();
+    }
+
+
+    }
+
+
+
+

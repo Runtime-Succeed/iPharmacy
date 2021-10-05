4359878f2a78077daf2da102f33d2e70a9218d64
gordan588888@gmail.com <JY8s9a7v2m!>
2021-10-05 13:06:24 -0700

    changed test for document to correct form

--- src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
+++ src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
@@ -1,17 +1,17 @@
 package com.example.iPharmacy;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IPharmacyApplicationTests {
 
-	public static void main(String[] args){
+	@Test
+	void documentTest(){
 		testDocument test = new testDocument("C:\\Users\\Jay\\Downloads\\HTN_Dosage_List.csv");
 		int size = test.questions.size();
-		if(size == 30)
-			System.out.println("Correct");
-		else throw new RuntimeException("Incorrect");
+		Assert.assertEquals(30, size);
 	}
 
 	@Test

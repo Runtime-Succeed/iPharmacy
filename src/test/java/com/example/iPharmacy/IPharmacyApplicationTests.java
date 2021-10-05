eb4bbbb821d1cc0fcc5b41b655df186e71b3a4c6
gordan588888@gmail.com <JY8s9a7v2m!>
2021-10-05 13:00:16 -0700

    added test for test document

--- src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
+++ src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
@@ -6,8 +6,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IPharmacyApplicationTests {
 
+	public static void main(String[] args){
+		testDocument test = new testDocument("C:\\Users\\Jay\\Downloads\\HTN_Dosage_List.csv");
+		int size = test.questions.size();
+		if(size == 30)
+			System.out.println("Correct");
+		else throw new RuntimeException("Incorrect");
+	}
+
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }

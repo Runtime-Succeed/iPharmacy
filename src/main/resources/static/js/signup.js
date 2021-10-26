c3e4eab4d432a25165cf3cafffd621e8de14ded4
Chen <89332989+ClorisXue93@users.noreply.github.com>
2021-10-26 13:40:19 -0700

    updata login and sign up with fetch

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -12,3 +12,47 @@ function checkPassword() {
         button.disabled = false;
     }
 }
+
+function allValid() {
+    var first = document.getElementById('firstName');
+    var last = document.getElementById('lastName');
+    var user = document.getElementById('username');
+    var email = document.getElementById('email');
+    var pass = document.getElementById('pswd');
+    var conf = document.getElementById('confirm');
+
+    if (first.checkValidity() && last.checkValidity() && user.checkValidity() && email.checkValidity()
+    && pass.checkValidity() && conf.checkValidity()) {
+        return true;
+    }
+    else
+        return false;
+
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    var button = document.getElementById("sub");
+    button.onclick = async(e) => {
+        data = {
+             "firstname": document.getElementById('firstName').value,
+             "lastname": document.getElementById('lastName').value,
+             "username": document.getElementById('username').value,
+             "email": document.getElementById('email').value,
+             "password": document.getElementById('pswd').value
+        }
+        console.log(data);
+        if(allValid()){
+            let response = await fetch('localhost:8080/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body:JSON.stringify(data),
+            })
+            .then(response => response.json())
+            .then(data => console.log(data));
+        }
+    }
+})
+
+

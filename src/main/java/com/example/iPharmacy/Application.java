0c30257060826062b9a2e2c0f1af27c9e882f700
gordan588888@gmail.com <JY8s9a7v2m!>
2021-11-16 12:41:34 -0800

    file uploading backend

--- src/main/java/com/example/iPharmacy/Application.java
+++ src/main/java/com/example/iPharmacy/Application.java
@@ -2,13 +2,23 @@ package com.example.iPharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
+import com.example.iPharmacy.utility.FileStorageService;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
+
+	@Resource
+	FileStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.init();
+	}
 }
 

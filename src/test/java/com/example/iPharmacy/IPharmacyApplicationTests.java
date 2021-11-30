90aa0433d21682f436b33d7dcbc8d62940ff8765
anthonyz713 <78340967+anthonyz713@users.noreply.github.com>
2021-11-29 18:41:52 -0800

    Changed tests

--- src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
+++ src/test/java/com/example/iPharmacy/IPharmacyApplicationTests.java
@@ -1,53 +1,16 @@
 package com.example.iPharmacy;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.apache.commons.codec.binary.Hex;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.iPharmacy.controllers.TestingController;
-import com.example.iPharmacy.data.QuestionSet;
-import com.example.iPharmacy.database.CustomUserInfoRepository;
 import com.example.iPharmacy.security.UserInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-@WebMvcTest(TestingController.class)
+@SpringBootTest
 public class IPharmacyApplicationTests {
-	
-	@Autowired
-	private MockMvc mvc;
-	
-	@MockBean
-	private CustomUserInfoRepository userRepo;
-	
-	/**
-	 * Ensures GET endpoint is reachable and QuestionSet structure works properly
-	 * @throws Exception
-	 */
-	@Test
-	public void testGetExampleQuestionSet() throws Exception {
-		MvcResult result = mvc.perform(
-				MockMvcRequestBuilders.get("/example/questionSet").accept(MediaType.APPLICATION_JSON)
-				).andReturn();
-		
-		QuestionSet qs = new ObjectMapper().readValue(result.getResponse().getContentAsString(), QuestionSet.class);
-		Assertions.assertEquals("HTN Dosage List", qs.getTitle());
-		Assertions.assertEquals(30, qs.getRows());
-		Assertions.assertEquals("Generic", qs.getQuestionAsk());
-		Assertions.assertEquals(
-				new ArrayList<String>(Arrays.asList("Brand", "Dose (mg)", "Max Dose (mg)")), 
-				qs.getAnswerCols());
-	}
 
 	/**
 	 * Ensures UserInfo hashes passwords correctly
